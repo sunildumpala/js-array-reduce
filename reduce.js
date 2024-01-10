ec6371b7cbe6d78cc56566179846759b9094384d
sunildumpala <dumpala@gmail.com>
2024-01-10 15:04:53 -0800

    squareAndFindEvens function refactored with =>

--- reduce.js
+++ reduce.js
@@ -156,3 +156,23 @@ function double(arr) {
 */
 
 let double =arr => arr.map(val => val *2);
+
+/**
+ * Refactor the following:
+ *  function squareAndFindEvens(numbers){
+  var squares = numbers.map(function(num){
+    return num ** 2;
+  });
+  var evens = squares.filter(function(square){
+    return square % 2 === 0;
+  });
+  return evens;
+}
+ */
+
+
+function squareAndFindEvens(numbers){
+    let squares = numbers.map(num => num ** 2 );
+    let evens = squares.filter(square=> square % 2 === 0 );
+    return evens;
+  }
